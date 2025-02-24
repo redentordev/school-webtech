eb8dcecc6588f3b7d54a5d7e80ab783fd02fc7a1
redentor <redenvalerio2@gmail.com>
2025-02-24 23:08:12 +0800

    Update meta title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Instagram Clone',
+  title: 'Picwall',
   description: 'A simple Instagram clone built with Next.js',
 };
 
